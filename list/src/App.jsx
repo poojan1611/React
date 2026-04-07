6c2a1fe81fe149f95db3c3eeb6ba405b979ad83b
Poojan <poojansheth999@gmail.com>
2026-04-07 21:11:04 +0530

    list

---
+++ list/src/App.jsx
@@ -0,0 +1,12 @@
+import List from './List';
+
+function App(){
+  return(
+    <div>
+      {/* <List /> */}
+      <List key={student.id} id={student.id} name={student.name} />
+    </div>
+  );
+}
+
+export default App;
