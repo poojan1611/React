837716567095d5211a126c8a9acac4e0b33e29e7
Poojan <poojansheth999@gmail.com>
2026-04-09 09:47:26 +0530

    n

--- list/src/List.jsx
+++ list/src/List.jsx
@@ -1,12 +1,37 @@
 function List({id, name}){
 
-    const users = ["Poojan", "Aum", "Agam", "Vimal"];
+    // const users = ["Poojan", "Aum", "Agam", "Vimal"];
+
+    // return(
+    //     <div>
+    //         <h2>Users List</h2>
+    //         <ul>
+    //             {/* {users.map((user, index) => (<li key ={index}>{user}</li>))} */}
+    //             {users.map((user) => ( <li>{user}</li> ))}
+    //         </ul>
+    //     </div>
+    // );
+
+    const students = [
+        {id : 1, name: "Poojan", age: 19, grade: "A", semester: "6th"},
+        {id : 2, name: "Aum", age: 21, grade: "A+", semester: "5th"},
+        {id : 3, name: "Agam", age: 20, grade: "B", semester: "4th"},
+        {id : 4, name: "Vimal", age: 18, grade: "A", semester: "3th"},
+    ];
 
     return(
         <div>
-            <h2>Users List</h2>
+            <h2>Student List</h2>
             <ul>
-                {users.map((user, index) => (<li key ={index}>{user}</li>))}
+                {students.map((student)=> (
+                    <div key = {student.id}>
+                        <h3>Name: {student.name}</h3>
+                        <p>Age: {student.age}</p>
+                        <p>Grade: {student.grade}</p>
+                        <p>Semester : {student.semester}</p>
+                        <hr />
+                    </div>
+                ) )}
             </ul>
         </div>
     );
