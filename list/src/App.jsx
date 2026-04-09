eac0590f0386ffbbb1d14301c19116828d5a6630
Poojan <poojansheth999@gmail.com>
2026-04-09 09:30:24 +0530

    o

--- list/src/App.jsx
+++ list/src/App.jsx
@@ -3,8 +3,7 @@ import List from './List';
 function App(){
   return(
     <div>
-      {/* <List /> */}
-      <List key={student.id} id={student.id} name={student.name} />
+      <List />
     </div>
   );
 }
--- list/src/List.jsx
+++ list/src/List.jsx
@@ -1,31 +1,16 @@
 function List({id, name}){
 
-    // const fruits = ["Apple", "Banana", "Mango", "Orange"];
+    const users = ["Poojan", "Aum", "Agam", "Vimal"];
 
-    // return(
-    //     <div>
-    //         <h2>Fruits List</h2>
-    //         <ul>
-    //             {fruits.map((fruit, index) => (<li key={index} > {fruit} </li>))}
-    //         </ul>
-    //     </div>
-    // );
+    return(
+        <div>
+            <h2>Users List</h2>
+            <ul>
+                {users.map((user, index) => (<li key ={index}>{user}</li>))}
+            </ul>
+        </div>
+    );
 
-    // ❌ Wrong — trying to use key as prop inside component
-            // function Student({ key, name }) {
-            // return <p>{key} - {name}</p>; // key is undefined here!
-            // }
-
-            // // ✅ Correct — pass id separately if needed
-            // function Student({ id, name }) {
-            return <p>{id} - {name}</p>;
-            // }
-
-            // // And in parent:
-            // <Student key={student.id} id={student.id} name={student.name} />
-
-        // And in parent:
-        <Student key={student.id} id={student.id} name={student.name} />
-    }
+}
 
 export default List;
